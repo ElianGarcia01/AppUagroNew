bec923ee35e59015e83cfd8b69b19c76ab71e8ab
Elian Garcia <telljua90@gmail.com>
2024-11-25 14:31:21 -0600

    despliegue exitoso

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react/',
+  base: '/AppUagroNew/',
   plugins: [react()],
   server: {
     port: 3000 // Cambia a un puerto disponible
